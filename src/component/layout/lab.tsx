7b7d8e1cb2b4f7f3ca51d3933908a6e89aed488e
maracodez <amarachiruhty4@gmail.com>
2026-06-18 10:22:18 +0100

    re-stying lab file

--- src/component/layout/lab.tsx
+++ src/component/layout/lab.tsx
@@ -42,7 +42,7 @@ const lab = () => {
 			}}
 			className="md:flex md:flex-wrap md:gap-x-4 md:mt-8"
 		>
-			<div className="bg-gradient-to-br from-[#130428] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-8 mt-12 md:w-[48%] flex gap-2 md:gap-4">
+			<div className="bg-gradient-to-br from-[#130428] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-8 mt-12 md:w-[48%] md:flex gap-2 md:gap-4">
 				<img 
 					src={tech} 
 					alt="" 
@@ -59,11 +59,11 @@ const lab = () => {
 					>Learn More</a>
 				</div>
 			</div>
-			<div className="bg-gradient-to-br from-[#1A0F2E] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-4 md:p-8 mt-12 md:w-[50%] flex gap-2 md:gap-4">
+			<div className="bg-gradient-to-br from-[#1A0F2E] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-4 md:p-8 mt-12 md:w-[50%] md:flex gap-2 md:gap-4">
 				<img 
 					src={task} 
 					alt="" 
-					className=" w-1/4 h-42"
+					className=" w-1/4 md:h-42"
 				/>
 				<div>
 					<h1 className="font-bold text-3xl md:text-4xl">Personal Project</h1>
@@ -76,14 +76,14 @@ const lab = () => {
 					>Learn More</a>
 				</div>
 			</div>
-			<div className="bg-gradient-to-br from-[#1A0F2E] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-4 md:p-8 mt-12 md:w-[48%] flex gap-2 md:gap-4">
+			<div className="bg-gradient-to-br from-[#1A0F2E] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-4 md:p-8 mt-12 md:w-[48%] md:flex gap-2 md:gap-4">
 				<img 
 					src={bglight} 
 					alt="" 
-					className="w-1/4 h-42"
+					className="w-1/4 md:h-42"
 				/>
 				<div>
-					<h1 className="font-bold text-2xl md:text-4xl">Portfolio Website</h1>
+					<h1 className="font-bold text-3xl md:text-4xl">Portfolio Website</h1>
 					<p className="md:leading-tight text-lg py-4">A responsive personal site showcasing projects, skills, and experience with clear UI, smooth navigation, and optimized performance.</p>
 					<a 
 						href="https://github.com/maracodez" 
@@ -93,11 +93,11 @@ const lab = () => {
 					>Learn More</a>
 				</div>
 			</div>
-			<div className="bg-gradient-to-br from-[#1A0F2E] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-8 mt-12 md:w-[50%] flex md:gap-4">
+			<div className="bg-gradient-to-br from-[#1A0F2E] to-[#21113A] border-2 border-[#2B1B4A] rounded-2xl shadow-lg p-8 mt-12 md:w-[50%] md:flex md:gap-4">
 				<img 
 					src={starred} 
 					alt="" 
-					className="w-1/4 h-42"
+					className=" w-1/4 md:h-42"
 				/>
 				<div>
 					<h1 className="font-bold text-3xl">E-Commerce Dashboard</h1>
