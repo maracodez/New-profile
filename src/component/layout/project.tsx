c548dc13f9fb1bc7101d518f299c5a859d9cf513
maracodez <amarachiruhty4@gmail.com>
2026-06-22 19:02:34 +0100

    wip

--- src/component/layout/project.tsx
+++ src/component/layout/project.tsx
@@ -19,9 +19,9 @@ const Project = () => {
                 <h4 className='text-purple-500 text-xl'>Featured Project</h4>
                 <h1
                    
-                    className='text-[3rem] md:text-4xl'
+                    className='text-[2.5rem] md:text-4xl'
                 >Fashion Hub</h1>
-                <p className='my-12  md:absolute md:right-[45%] md:z-10 bg-[#432043] backdrop-blur-md shadow-md p-10 leading-loose rounded-xl text-lg'>A web app for visualizing fashion data. View your various latest styles, materials, sewing equipements and detailed fashion information about each style. Add and purchase new items of recommended styles based on the existing trends and more.</p>
+                <p className='my-12  md:absolute md:right-[45%] md:z-10 bg-[#432043] md:bg-transparent backdrop-blur-md shadow-md p-10 leading-loose rounded-xl md:text-lg shadow-[0_0_30px_rgba(0,0,0,0.4)]'>A web app for visualizing fashion data. View your various latest styles, materials, sewing equipements and detailed fashion information about each style. Add and purchase new items of recommended styles based on the existing trends and more.</p>
                 <div className='flex md:absolute md:bottom-0 cursor-pointer'>
                     <motion.a 
                         whileHover={{ scale: 1.1 }}
@@ -75,7 +75,7 @@ const Project = () => {
             <div className='hidden md:block w-1/2 text-right md:relative'>
                 <h4 className='text-purple-500'>Featured Project</h4>
                 <h1 className='text-[3rem]'>Dog Breed</h1>
-                <p className='my-14  md:absolute md:left-[-9%] md:z-10 bg-[#432043] backdrop-blur-md shadow-md p-10 leading-loose rounded-xl text-left'>A web app for visualizing animal(dog breed) specie. View your dog breeds, their specie, images, names and detailed information about each breed. Search desired dog breed which are not showcased on the dashboard and more.</p>
+                <p className='my-14  md:absolute md:left-[-9%] md:z-10 bg-transparent backdrop-blur-md shadow-md p-10 leading-loose rounded-xl text-left shadow-[0_0_30px_rgba(0,0,0,0.4)]'>A web app for visualizing animal(dog breed) specie. View your dog breeds, their specie, images, names and detailed information about each breed. Search desired dog breed which are not showcased on the dashboard and more.</p>
                 <div 
                     className='flex md:absolute md:bottom-0 md:right-0 cursor-pointer'
                 >
